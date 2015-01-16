289e691c616a78862945a2d77b617aaf03f2ab13
JosephClay <j053phclay@gmail.com>
2015-01-16 16:47:49 -0600

    build

--- signal.js
+++ signal.js
@@ -1,6 +1,6 @@
-/*! signal-js - v0.0.7 - 2014-11-27 %>
+/*! signal-js - v0.1.0 - 2015-01-16 %>
  * https://github.com/JosephClay/signal-js
- * Copyright (c) 2013-2014 Joe Clay; License: MIT */
+ * Copyright (c) 2013-2015 Joe Clay; License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.signal=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var undefined, // safe undef
 	_splice = [].splice,
@@ -136,12 +136,6 @@ var klassExtend = function(constructor, extension) {
 	return fn;
 };
 
-var create = function() {
-	var signal = new Signal();
-	signal.extend = klassExtend;
-	return signal;
-};
-
 function Signal() {
 	/**
 	 * Holds active events by handle + event + namespace
@@ -167,13 +161,6 @@ Signal.extend = klassExtend;
 Signal.prototype = {
 
 	constructor: Signal,
-
-	/**
-	 * Returns a new signal instance
-	 * @return {signal}
-	 */
-	construct: create,
-	create: create,
 
 	subscribe: function(name, func) {
 		var subscriptions = this._subs || (this._subs = {});
@@ -340,9 +327,20 @@ Signal.prototype = {
 	}
 };
 
+var create = function() {
+	var s = new Signal();
+	s.prototype = Signal.prototype;
+	s.extend = klassExtend;
+	return s;
+};
+
 // Create a pub/sub to expose signal as
 // e.g. signal.on(), signal.trigger()
-var signal = new Signal();
+var signal = _extend(create, create());
+signal.prototype = Signal.prototype;
+
+// setup create methods
+signal.create = create;
 
 // setup extension method
 signal.extend = klassExtend;
