b73154af45676fae5ea649e24e5952b0ff56f06d
JosephClay <j053phclay@gmail.com>
2021-10-18 12:07:42 -0500

    move utils

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const isFunction = value => typeof value === 'function';
+
+export const isString = value => typeof value === 'string';
+
+export const isSymbol = value => typeof value === 'symbol';
