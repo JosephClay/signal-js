3c3b2ce10eee2f46c075227e9d5f603befce2b3e
MarkosTh09 <markos.theocharis2009@gmail.com>
2021-12-24 10:10:39 +0200

    Corrected more types

--- src/Signal.d.ts
+++ src/Signal.d.ts
@@ -1,16 +1,16 @@
 type listener = (
-    name: string,
-    fn: (...args: any[]) => any) => Signal
+    eventName: string,
+    listener: (...args: any[]) => any) => Signal
 
 export interface Signal {
     on: listener
     off: listener
     once: listener
-    emit: (name: string, ...arg?: any[]) => Signal
+    emit: (eventName: string, ...arg?: any[]) => Signal
     listeners: (name: string) => Signal
     names: () => string[]
     size: () => number
-    clear: (name: string) => Signal
+    clear: (eventName: string) => Signal
 }
 declare const proto: Signal
 
